4b54e4457fd82dbcbc0b747ec838498db41590b1
Kuxry <likaipeng666@gmail.com>
2025-01-12 14:04:45 +0900

    update102leetcode

--- leetcode226-invert-binary-tree.cpp
+++ leetcode226-invert-binary-tree.cpp
@@ -1,3 +1,14 @@
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
